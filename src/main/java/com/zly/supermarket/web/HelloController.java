3a58d15ef1a7d454964cbf023dcdd5f006e868fa
cifz <wangzhen1798@gmail.com>
2017-12-23 11:23:37 +0800

    132

--- src/main/java/com/zly/supermarket/web/HelloController.java
+++ src/main/java/com/zly/supermarket/web/HelloController.java
@@ -3,6 +3,7 @@ package com.zly.supermarket.web;
 import com.alibaba.fastjson.JSON;
 import com.zly.supermarket.data.UserInfo;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -10,13 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @RequestMapping("/hello")
-    public String index(){
+    public String index(@RequestParam String name){
+
         UserInfo userInfo = new UserInfo();
         userInfo.setCode(1);
         userInfo.setMsg("请求成功");
 
         UserInfo.Data data = new UserInfo.Data();
-        data.setName("李江明");
+        data.setName(name);
         data.setAge(24);
 
         userInfo.setInfo(data);
